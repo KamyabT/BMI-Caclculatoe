3afc62cf95d8b29ea6374033a8b0f1fc824b9bd0
KamyabT <dgghost034@gmail.com>
2023-02-20 20:06:59 +0330

    BMI Calculator With Input

--- src/Component/Maincontent.jsx
+++ src/Component/Maincontent.jsx
@@ -1,6 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 const Maincontent = () => {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+
+  let bmiCalc = () => {
+    let BmiRes = (weight / (height * height));
+    setBmi(BmiRes);
+  };
+
   return (
     <main className="mainDesign bg-warning d-flex justify-content-center align-items-center">
       <div className="calculatorMainDesign">
@@ -10,38 +19,31 @@ const Maincontent = () => {
         <div className="bg-white p-3">
           <div className="sliderContainer d-flex flex-column align-items-center">
             <div>
-              <span>Weight :</span>
-              <span id="weightVal">10</span>
+              <span>Weight : </span>
+              <span id="weightVal">{weight}</span>
             </div>
             <input
-              type="range"
-              min="40"
-              max="250"
-              step="1"
-              // value="50"
-              // value="50"
-              className="slider my-3"
-              // id="myRange"
+              className="w-100 my-3"
+              type="text"
+              onChange={(event) => setWeight(event.target.value)}
             ></input>
           </div>
           <div className="sliderContainer d-flex flex-column align-items-center">
             <div>
-              <span>Height :</span>
-              <span id="heightVal">10</span>
+              <span>Height : </span>
+              <span id="Hvalue">{height}</span>
             </div>
             <input
-              type="range"
-              min="40"
-              max="250"
-              step="1"
-              // value="50"
-              // value="50"
-              className="slider my-3"
-              // id="myRange"
+              className="w-100 my-3"
+              type="text"
+              onChange={(event) => setHeight(event.target.value)}
             ></input>
           </div>
-          <div className="d-flex justify-content-center mt-2">
-            <button className="btn btn-dark">result</button>
+          <div className="d-flex flex-column justify-content-center mt-2">
+            <button className="btn btn-dark" onClick={bmiCalc}>
+              Calculate my BMI
+            </button>
+            <p className="text-center mt-3">{bmi}</p>
           </div>
         </div>
       </div>
